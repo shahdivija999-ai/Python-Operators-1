1ea1c44b669ef42e167df6bbb998bf5d40416639
shahdivija999-ai <shahdivija999@gmail.com>
2026-05-06 17:12:37 -0500

    Add files via upload

---
+++ Activity3.py
@@ -0,0 +1,10 @@
+print("Enter Marks Obtained in 4 Subjects")
+
+Math=int(input ("Maths"))
+English=int(input ("English")) 
+Science=int(input ("Science"))
+Hindi=int(input ("Hindi"))
+
+Sum = Math+English+Science+Hindi
+Percentage = (Sum/400) *100
+print("Percentage is", Percentage)
